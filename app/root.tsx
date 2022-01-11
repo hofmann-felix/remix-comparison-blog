de7a51ca064582a63eb02ec475182aeabd133de3
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2022-01-11 20:13:41 +0100

    fix: render styled-components correctly

--- app/root.tsx
+++ app/root.tsx
@@ -7,24 +7,22 @@ import {
   ScrollRestoration
 } from "remix";
 import type { MetaFunction } from "remix";
-import StylesContext from "./StylesContext";
-import {useContext} from "react";
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
 
 export default function App() {
-  const styles = useContext(StylesContext);
-
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
-        {styles}
         <Links />
+        {typeof document === "undefined"
+          ? "__STYLES__"
+          : null}
       </head>
       <body>
         <Outlet />
